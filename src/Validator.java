cb5b3541ea9480ccf23c6468720e2f29bad3f6ba
Leito131 <brea.couceiro.paulo.ald@gmail.com>
2023-04-20 21:12:34 +0200

    Modificado validador

--- src/Validator.java
+++ src/Validator.java
@@ -1,21 +1,28 @@
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Validator {
     public static boolean validarEmail(String email) {
+        boolean valido = false;
         if (email == null || email.isEmpty()) {
-            return false;
+            System.err.println("El campo e-mail está vacío.");
+            return valido;
         }
         int arroba = email.indexOf('@');
         int punto = email.lastIndexOf('.');
         if (arroba < 1 || punto < arroba + 2 || punto + 2 > email.length()) {
-            return false;
+            System.err.println("El e-mail introducido tiene el formato correcto.");
+            return valido;
         }
-        return true;
+        valido = true;
+        return valido;
     }
 
-    public static String validarContrasinal(String contrasinal) {
+    public static boolean validarContrasinal(String contrasinal) {
+        boolean valido = false;
         if (contrasinal == null || contrasinal.length() < 8 ) {
-            return "La contraseña no es lo suficientemente larga.";
+            System.err.println("La contraseña no es lo suficientemente larga.");
+            return valido;
         }
         boolean tieneNumero = false;
         boolean tieneLetra = false;
@@ -30,27 +37,49 @@ public class Validator {
                 tieneCaracterEspecial = true;
             }
         }
-        return "tieneNumero: " + tieneNumero + " | tieneLetra: " + tieneLetra + " | tieneLetra: " + tieneCaracterEspecial;
+
+        if (!tieneNumero || !tieneLetra || !tieneCaracterEspecial){
+            System.err.println("La contraseña debe contener un número, una letra y un caracter especial.");
+            return valido;
+        }else{
+            valido = true;
+            return valido;
+        }
     }
 
     public static boolean validarWeb(String web) {
         if (web == null || web.isEmpty()) {
+            System.err.println("El campo web está vacío.");
             return false;
         }
         try {
             new java.net.URL(web).toURI();
             return true;
         } catch (Exception e) {
+            System.err.println("La web introducida no tiene el formato correcto.");
             return false;
         }
     }
 
-    public static boolean validarNacemento(Date nacemento) {
+    public static boolean validarNacemento(LocalDate nacemento) {
+        boolean valido = false;
         if (nacemento == null) {
-            return false;
+            System.err.println("El campo nacimiento está vacío.");
+            return valido;
         }
-        Date dataActual = new Date();
-        return nacemento.before(dataActual);
+
+        LocalDate dataActual =  LocalDate.now();
+
+        if (!nacemento.isBefore(dataActual)){
+            System.err.println("La fecha introducida no es válida.");
+            return valido;
+        }else{
+            valido = true;
+            return valido;
+        }
+
+        
+         
     }
 
 }
